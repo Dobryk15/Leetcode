7d72c89602981e6e41d57e91210e3c8fb398cb6c
Dobryk15 <dobrovolska75@ukr.net>
2017-07-14 19:24:38 +0300

    Update task289.cpp

--- task289.cpp
+++ task289.cpp
@@ -7,9 +7,7 @@ void gameOfLife(vector<vector<int>>& board)
 	if (board.size() == 0)
 		return;
 	vector<vector<int>> b = board;
-	int n = board.size(), m = board[0].size();
-	
-	
+	int n = board.size(), m = board[0].size();	
 	if (m == 1)
 	{
 		if (b[0][0] == 1)
@@ -69,7 +67,6 @@ void gameOfLife(vector<vector<int>>& board)
 			board[n - 2][m - 2] += 1;
 			board[n - 1][m - 1] += 10;
 		}
-
 		return;
 	}
 }
@@ -80,13 +77,10 @@ int main()
 	myBoard[0].push_back(1);
 	myBoard[0].push_back(0);
 	gameOfLife(myBoard);
-
 	for (int i = 0; i < 1; ++i)
 	{
 		for (int j = 0; j < 2; ++j)
-		{
-			cout << myBoard[i][j] << " ";
-		}
+  			cout << myBoard[i][j] << " ";
 		cout << endl;
 	}
 	system("pause");
